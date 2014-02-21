0ac47d6daf03844baa523d382bdb17b7051620f9
Renato Cavalcanti <renato@xavena.eu>
2014-02-21 16:38:41 +0100

    ActiveRecord based with DaoProvider function

--- src/main/scala/slick/dao/ActiveRecord.scala
+++ src/main/scala/slick/dao/ActiveRecord.scala
@@ -1,13 +1,17 @@
 package slick.dao
 
+import scala.slick.jdbc.JdbcBackend
 
-abstract class ActiveRecord[R, I](val row: R, val dao:SlickDao[R, I]) {
+abstract class ActiveRecord[R, I](val row: R, val daoProvider: JdbcBackend#Session => SlickDao[R, I]) {
 
-  def save: R = dao.save(row)
-  def delete: Boolean = {
+  def save(implicit session:JdbcBackend#Session): R = daoProvider(session).save(row)
+
+  def delete(implicit session:JdbcBackend#Session): Boolean = {
+    val dao = daoProvider(session)
     dao.extractId(row) match {
       case Some(id) => dao.deleteById(id)
       case None => false
     }
   }
+
 }
