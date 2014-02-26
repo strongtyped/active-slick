ffdacbc956c21e0167a5ffdf30d45e8bd48cbb3e
Renato Cavalcanti <renato@xavena.eu>
2014-02-26 08:29:45 +0100

    added license + copyright

--- src/main/scala/slick/dao/ActiveRecord.scala
+++ src/main/scala/slick/dao/ActiveRecord.scala
@@ -1,13 +1,29 @@
+/*
+ * Copyright 2014 Renato Guerra Cavalcanti (@renatocaval)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package slick.dao
 
 import scala.slick.jdbc.JdbcBackend
 
 abstract class ActiveRecord[M, I](val model: M) {
 
-  def save(implicit session:JdbcBackend#Session): M =
+  def save(implicit session: JdbcBackend#Session): M =
     daoProvider(session).save(model)
 
-  def delete(implicit session:JdbcBackend#Session): Boolean =
+  def delete(implicit session: JdbcBackend#Session): Boolean =
     daoProvider(session).delete(model)
 
   def daoProvider(session: JdbcBackend#Session): SlickDao[M, I]
