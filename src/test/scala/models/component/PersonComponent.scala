4ee3a205ec82cd431525a565f27afa515b59205c
Olivier Samyn <code@oleastre.be>
2014-03-09 23:30:12 +0100

    Introduce IdentifiableJdbcDao that defines the add method for IdentifiableTable.

--- src/test/scala/models/component/PersonComponent.scala
+++ src/test/scala/models/component/PersonComponent.scala
@@ -16,7 +16,7 @@
 
 package models.component
 
-import slick.dao.{ActiveRecord, SlickJdbcDao, IdentifiableTable}
+import slick.dao.{ActiveRecord, IdentifiableJdbcDao, IdentifiableTable}
 import models.Person
 import scala.slick.jdbc.JdbcBackend
 import scala.slick.lifted
@@ -26,7 +26,7 @@ trait PersonComponent {
 
   val profile:JdbcProfile
 
-  class PersonDao(implicit val session:JdbcBackend#Session) extends SlickJdbcDao[Person, Int] {
+  class PersonDao(implicit val session:JdbcBackend#Session) extends IdentifiableJdbcDao[Person, Int] {
 
     val profile = PersonComponent.this.profile
     import profile.simple._
@@ -50,8 +50,6 @@ trait PersonComponent {
 
     def queryById(id: Int) = query.filter(_.id === id)
     
-    def add(person: Person) = query.returning(query.map(_.id)).insert(person)
-
     def create() =  query.ddl.create
   }
 
