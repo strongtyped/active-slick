a56aa5e4f22d3a9b7d077064c7ec5274db2709fd
Renato Cavalcanti <renato@strongtyped.io>
2014-07-06 22:31:10 +0200

    Some initial documentation (more to come)

--- src/test/scala/io/strongtyped/active/slick/DB.scala
+++ src/test/scala/io/strongtyped/active/slick/DB.scala
@@ -7,7 +7,7 @@ import scala.slick.driver.H2Driver.simple._
 object DB {
 
   val db = {
-    val db = Database.forURL("jdbc:h2:mem:hello", driver = "org.h2.Driver")
+    val db = Database.forURL("jdbc:h2:mem:active-slick", driver = "org.h2.Driver")
     val keepAliveSession = db.createSession()
     keepAliveSession.force() // keep the database in memory with an extra connection
     db.withTransaction { implicit session =>
---
+++ src/test/scala/io/strongtyped/active/slick/docexamples/MappingActiveSlickIdentifiable.scala
@@ -0,0 +1,50 @@
+package io.strongtyped.active.slick.docexamples
+
+import io.strongtyped.active.slick.ActiveSlick
+import io.strongtyped.active.slick.models.Identifiable
+
+import scala.slick.driver.{H2Driver, JdbcDriver}
+
+trait MappingActiveSlickIdentifiable {
+  this: ActiveSlick =>
+
+  import jdbcDriver.simple._
+
+  case class Foo(name: String, id: Option[Int] = None) extends Identifiable[Foo] {
+    override type Id = Int
+    override def withId(id: Id): Foo = copy(id = Some(id))
+  }
+
+  class FooTable(tag: Tag) extends IdTable[Foo, Int](tag, "FOOS") {
+    def name = column[String]("NAME")
+    def id = column[Int]("ID", O.PrimaryKey, O.AutoInc)
+    def * = (name, id.?) <>(Foo.tupled, Foo.unapply)
+  }
+
+  val Foos = TableQuery[FooTable]
+
+  /**
+   * Class Extension for {{{TableQuery[FooTable]}}}. Will provide basic CRUD operations
+   * @param fooQuery
+   */
+  implicit class FooQueryExtension(fooQuery: TableQuery[FooTable]) extends IdTableExt[Foo](fooQuery)
+}
+
+
+object MappingActiveSlickIdentifiableApp {
+
+  class Components(override val jdbcDriver: JdbcDriver) extends ActiveSlick with MappingWithActiveSlick {
+    import jdbcDriver.simple._
+    val db = Database.forURL("jdbc:h2:mem:active-slick", driver = "org.h2.Driver")
+  }
+  object Components { val instance = new Components(H2Driver) }
+  import Components.instance._
+
+  def main(args:Array[String]) : Unit = {
+    db.withTransaction { implicit sess =>
+      val foo = Foo("foo")
+      val fooWithId : Foo = Foos.save(foo)
+      assert(fooWithId.id.isDefined, "Foo's ID should defined")
+    }
+  }
+}
