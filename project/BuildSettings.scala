c153d828fa7d51afb5f0a7259aac12e9caf2fc2f
Renato Cavalcanti <renato@strongtyped.io>
2014-10-14 22:18:43 +0200

    sonatype plugin

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -1,6 +1,8 @@
 import sbt._
 import sbt.Keys._
 import com.typesafe.sbt.SbtScalariform
+import xerial.sbt.Sonatype.SonatypeKeys._
+import xerial.sbt.Sonatype.sonatypeSettings
 
 trait BuildSettings { this:Build => 
 
@@ -16,14 +18,9 @@ trait BuildSettings { this:Build =>
 		, scalacOptions 	:= ScalacOptions
 	) ++ SbtScalariform.scalariformSettings ++ sonatypePublishSettings
 
-  	def sonatypePublishSettings = Seq(
-	  publishMavenStyle := true,
-	  publishTo := {
-		val nexus = "https://oss.sonatype.org/"
-		if (isSnapshot.value) Some("snapshots" at nexus + "content/repositories/snapshots")
-		else Some("releases"  at nexus + "service/local/staging/deploy/maven2")
-	  },
-	  pomExtra 		:= (<url>https://github.com/strongtyped/active-slick</url>
+  	def sonatypePublishSettings = sonatypeSettings ++ Seq(
+  	  profileName		:= "io.strongtyped",
+	  pomExtra 			:= (<url>https://github.com/strongtyped/active-slick</url>
       <licenses>
         <license>
           <name>Apache-style</name>
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -2,4 +2,6 @@ addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.5.2")
 
 addSbtPlugin("com.typesafe.sbt" % "sbt-scalariform" % "1.3.0")
 
+addSbtPlugin("org.xerial.sbt" % "sbt-sonatype" % "0.2.1")
+
 addSbtPlugin("com.typesafe.sbt" % "sbt-pgp" % "0.8.3")
