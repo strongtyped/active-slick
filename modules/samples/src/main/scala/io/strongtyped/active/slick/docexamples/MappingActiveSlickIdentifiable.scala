6ce8672fefb593e2a728c49c90a269d60d1db454
Renato Cavalcanti <renato@strongtyped.io>
2015-08-07 02:44:43 +0200

    asciidoctor

--- modules/samples/src/main/scala/io/strongtyped/active/slick/docexamples/MappingActiveSlickIdentifiable.scala
+++ modules/samples/src/main/scala/io/strongtyped/active/slick/docexamples/MappingActiveSlickIdentifiable.scala
@@ -3,59 +3,38 @@ package io.strongtyped.active.slick.docexamples
 import _root_.io.strongtyped.active.slick._
 import slick.driver.H2Driver
 
-import scala.concurrent.Await
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.duration._
 import scala.language.postfixOps
 
+//@formatter:off
+object MappingActiveSlickIdentifiable extends JdbcProfileProvider {
 
-object MappingActiveSlickIdentifiable extends  JdbcProfileProvider {
-
+  // tag::adoc[]
   val jdbcProfile = H2Driver
+
   import jdbcProfile.api._
 
   case class Foo(name: String, id: Option[Int] = None) extends Identifiable {
     override type Id = Int
   }
 
-  object FooActions$$$ extends EntityActions[Foo](jdbcProfile) {
+  object FooRepository extends EntityActions[Foo](jdbcProfile) {
 
     class FooTable(tag: Tag) extends jdbcProfile.api.Table[Foo](tag, "FOOS") {
       def name = column[String]("NAME")
       def id = column[Int]("ID", O.PrimaryKey, O.AutoInc)
       def * = (name, id.?) <>(Foo.tupled, Foo.unapply)
     }
-    type EntityTable = FooTable
 
+    type EntityTable = FooTable // # <1>
 
-    def $id(table:FooTable) = table.id
+    def $id(table: FooTable) = table.id // # <2>
 
-    val tableQuery = TableQuery[FooTable]
+    val tableQuery = TableQuery[FooTable] // # <3>
 
-    val idLens = SimpleLens[Foo, Option[Int]](_.id, (foo, id) => foo.copy(id = id))
+    val idLens = SimpleLens[Foo, Option[Int]](_.id, (foo, id) => foo.copy(id = id)) // # <4>
 
-
-    def createSchema = {
-      import jdbcProfile.api._
-      tableQuery.schema.create
-    }
   }
 
-  def run[T](block: Database => T): T = {
-    val db = Database.forURL("jdbc:h2:mem:active-slick", driver = "org.h2.Driver")
-    try {
-      Await.ready(db.run(FooActions$$$.createSchema), 200 millis)
-      block(db)
-    } finally {
-      db.close()
-    }
-  }
-
-  def main(args: Array[String]): Unit = {
-    run { db =>
-      val foo = Foo("foo")
-      val fooWithId = Await.result(db.run(FooActions$$$.save(foo)), 200 millis)
-      assert(fooWithId.id.isDefined, "Foo's ID should be defined")
-    }
-  }
+  // end::adoc[]
 }
+//@formatter:on
--- modules/samples/src/main/scala/io/strongtyped/active/slick/docexamples/MappingWithoutActiveSlick.scala
+++ modules/samples/src/main/scala/io/strongtyped/active/slick/docexamples/MappingWithoutActiveSlick.scala
@@ -1,37 +1,26 @@
 package io.strongtyped.active.slick.docexamples
 
-import scala.concurrent.Await
-import scala.concurrent.duration._
 import scala.language.postfixOps
-import scala.concurrent.ExecutionContext.Implicits.global
 
+//@formatter:off
 object MappingWithoutActiveSlick {
 
+  // tag::adoc[]
   import slick.driver.H2Driver.api._
 
-  case class Foo(name: String, id: Option[Int] = None)
+  case class Foo(name: String, id: Option[Int] = None) // #<1>
 
   class FooTable(tag: Tag) extends Table[Foo](tag, "FOOS") {
     def name = column[String]("NAME")
-
-    def id = column[Int]("ID", O.PrimaryKey, O.AutoInc)
-
-    def * = (name, id.?) <> (Foo.tupled, Foo.unapply)
+    def id = column[Int]("ID", O.PrimaryKey, O.AutoInc) // #<2>
+    def * = (name, id.?) <>(Foo.tupled, Foo.unapply)
   }
 
   val Foos = TableQuery[FooTable]
 
-  val db = Database.forURL("jdbc:h2:mem:active-slick", driver = "org.h2.Driver")
-  try {
+  val foo = Foo("foo")
+  val insertAction = Foos.returning(Foos.map(_.id)) += foo // #<3>
 
-    Await.ready(db.run(Foos.schema.create), 200 millis)
-    val foo = Foo("foo")
-    val insertAction = db.run(Foos.returning(Foos.map(_.id)) += foo)
-    val id = Await.result(insertAction, 200 millis)
-    foo.copy(id = Some(id))
-
-  } finally {
-    db.close()
-  }
-
+  // end::adoc[]
 }
+//@formatter:on
